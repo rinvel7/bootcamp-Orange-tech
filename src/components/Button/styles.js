a0b624e6e25f2825c67cd17f1b19903e2dfa32a2
rinvel7 <rinavelasquez03@gmail.com>
2022-12-08 18:58:55 -0400

    finish

---
+++ src/components/Button/styles.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    padding: 10px;
+    margin: 2px 5px 2px 0;
+    border: 1px solid #CDCDCD;
+    background-color: #AC398C;
+    border-radius: 20%;
+    color: #FFFFFF;
+    font-size: 24px;
+    font-weight: 700;
+    flex: 1;
+    
+    &:hover {
+        opacity: 0.6;
+    }
+`
+
+export const ButtonSpecial = styled.button`
+    padding: 5px 2px;
+    margin: 5px;
+    border: 1px solid #CDCDCD;
+    background-color: #3D2267;
+    border-radius: 20%;
+    color: #FFFFFF;
+    font-size: 24px;
+    font-weight: 700;
+    flex: 1;
+    
+    &:hover {
+        opacity: 0.6;
+    }
+`
---
+++ src/styles.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+
+export const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+    background-color: #FFFFFF;
+    
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+
+export const Content = styled.div`
+    background: #3E70BB; 
+    padding: 10px;
+    margin: 35%;
+    width: 50%;
+`
+
+export const Row = styled.div`
+    display:flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+`
+
+export const Column = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+`
+
